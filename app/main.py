98e0c35a28e7154940ecf8092021fdc004efad46
pillaravindar <pillaravindar90@gmail.com>
2026-06-01 16:52:08 +0530

    Initial Project Setup

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="FASTAPI task manager",
+    description="A simple task manager API build for DevOps project"
+    version="1.0"
+)
 
 @app.get("/")
 def root():
     return {"message": "Hello Internship Project"}
+
+@app.get("/health")
+def health_check():
+    return{
+        status:"active",
+        message:"Application is running "
+    }
