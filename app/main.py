b06971f5fc2b7661fa98f434c46d64d9ef8f8760
pillaravindar <pillaravindar90@gmail.com>
2026-06-03 19:30:51 +0530

    Initial Project Setup

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
+from app.schemas import TaskCreate,TaskResponse
 
 app = FastAPI(
     title="FASTAPI task manager",
     description="A simple task manager API build for DevOps project",
     version="1.0"
 )
+tasks=[]
+task_id_counter=1
 
 @app.get("/")
 def root():
@@ -16,3 +19,22 @@ def health_check():
         "status":"active",
         "message":"Application is running "
     }
+
+
+@app.post("/tasks", response_model=TaskResponse)
+def create_task(task: TaskCreate):
+
+    global task_id_counter
+
+    new_task = {
+        "id": task_id_counter,
+        "title": task.title,
+        "description": task.description,
+        "completed": False
+    }
+
+    tasks.append(new_task)
+
+    task_id_counter += 1
+
+    return new_task
---
+++ app/schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class TaskCreate(BaseModel):
+    title:str
+    description : str | None = None
+
+class TaskResponse(BaseModel):
+    id:int
+    title:str
+    description :str | None = None
+    completed:bool=False
