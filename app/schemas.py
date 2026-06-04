fa3008d52f2525cf3c5cde6668da7a6b9516363d
pillaravindar <pillaravindar90@gmail.com>
2026-06-04 15:37:56 +0530

    Fixing Bugs and syntax

--- app/schemas.py
+++ app/schemas.py
@@ -1,11 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 
 class TaskCreate(BaseModel):
-    title:str
+    title:str=Field(...,min_length=1,max_length=100)
     description : str | None = None
 
 class TaskUpdate(BaseModel):
-    title:str
+    title:str=Field(...,min_length=1,max_length=100)
     description:str|None=None
     completed:bool
 class TaskResponse(BaseModel):
